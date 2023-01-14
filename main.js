6c07ad4306a59a4045724e67885aa8a06f851642
Seaoak <seaoak@seaoak.jp>
2023-01-14 14:15:49 +0900

    Implement creating two windows and correcting UserAgent

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const createWindow = () => {
+//===========================================================================
+function correctUserAgent(ua) {
+    // user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) tweenize/0.1.0 Chrome/108.0.5359.179 Electron/22.0.2 Safari/537.36
+    return ua.split(' ').filter(s => ! /^(tweenize|Electron)\//.test(s)).join(' ');
+}
+
+//===========================================================================
+function createWindow(filenameOfPreload, location) {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, filenameOfPreload),
         },
     });
 
-    win.loadFile('index.html')
-};
+    if (location.startsWith('https://')) {
+        win.loadURL(location);
+    } else {
+        win.loadFile(location);
+    }
 
-app.whenReady().then(() => {
-    createWindow();
+    return win;
+}
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
+//===========================================================================
+const winTable = {};
+
+function start() {
+    if (Object.isFrozen(winTable)) return;
+    winTable.origin = createWindow('preload_for_origin.js', 'https://twitter.com/');
+    winTable.list = createWindow('preload_for_list.js', 'index.html');
+    Object.freeze(winTable);
+}
+
+//===========================================================================
+app.userAgentFallback = correctUserAgent(app.userAgentFallback);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
+});
+
+app.whenReady().then(() => {
+    start();
 });
